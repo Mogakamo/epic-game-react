6cbeb8e7dd39a4de6d9d6b734b63791190ce6c88
Mogakamo <>
2021-11-09 14:32:35 +0000

    finshed for the day

--- src/Components/SelectCharacter/index.js
+++ src/Components/SelectCharacter/index.js
@@ -10,6 +10,20 @@ const SelectCharacter = ({setCharacterNFT}) => {
   const [gameContract, setGameContract] = useState(null)
 ;
 
+
+  const mintCharacterNFTAction = (characterId) => async() => {
+    try {
+      if(gameContract) {
+        console.log('Minting character in progress...');
+        const mintTxn = await gameContract.mintCharacterNFT(characterId);
+        await mintTxn.wait();
+        console.log('mintTxn:', mintTxn);
+      }
+    } catch (error) {
+      console.warn('MintCharacterAction Error:', error);
+    }
+  }
+
   // UseEffect
   useEffect(() => {
     const {ethereum} = window;
@@ -56,18 +70,53 @@ const SelectCharacter = ({setCharacterNFT}) => {
     }
   };
 
+  const onCharacterMint = async (sender, tokenId, characterIndex) => {
+    console.log(`CharacterNFTMinted - sender: ${sender} tokenId: ${tokenId.toNumber()} characterIndex: ${characterIndex.toNumber()}`);
+    
+    
+    if(gameContract) {
+      const characterNFT = await gameContract.checkIfUserHasNFT();
+      console.log('CharacterNFT: ', characterNFT);
+      setCharacterNFT(transformCharacterData(characterNFT))
+    }
+  }
+
   /*
    * If our gameContract is ready, let's get characters!
    */
   if (gameContract) {
     getCharacters();
+
+    gameContract.on('CharacterNFTMinted', onCharacterMint);
+  }
+  return () => {
+    if (gameContract) {
+      gameContract.off('CharacterNFTMinted', onCharacterMint);
+    }
   }
 }, [gameContract]);
 
 
+  // Render Methods
+const renderCharacters = () =>
+  characters.map((character, index) => (
+    <div className="character-item" key={character.name}>
+      <div className="name-container">
+        <p>{character.name}</p>
+      </div>
+      <img src={character.imageURI} alt={character.name} />
+      <button
+        type="button"
+        className="character-mint-button"
+        onClick={mintCharacterNFTAction(index)}
+      >{`Mint ${character.name}`}</button>
+    </div>
+  ));
+
   return (
     <div className="select-character-container">
       <h2>Mint your Hero. Choose wisely.</h2>
+      <div className="character-grid">{renderCharacters()}</div>
     </div>
   )
 }
